d5fe9a9cb1c2bceac22f724cdd8dbaf2abfe926b
BlackBond88 <78538203+BlackBond88@users.noreply.github.com>
2021-02-06 16:18:28 +0200

    Тестовая программа по классам

--- test.py
+++ test.py
@@ -1,33 +1,37 @@
+class Dragon:
+    def __init__(self, name):
+        self.name = name
+        self.health = 100
 
+    def is_alive(self):
+        return self.health > 0
 
-def dragon_create():
-    global health
-    health = 100
+    def get_damage(self, damage):
+        self.health -= damage
+        if self.health < 0:
+            self.health = 0
 
+    def talk(self):
+        print(self.name, 'health', self.health, '. Hit me!')
 
-def dragon_is_alive():
-    return health > 0
-
-
-def dragon_get_damage(damage):
-    global health
-    health -= damage
-    if health < 0:
-        health = 0
-
-def dracon_talk():
-    print('My health', health, '. Hit me!')
+    def final_cry(self):
+        print(self.name 'is dead...')
 
 
 def main():
-    dragon_create()
+    enemy_list = [Dragon('Smog'), Dragon('Hidra')]
     finish = False
     while not finish:
-        dracon_talk()
+        enemy = enemy_list[0]
+        enemy.talk()
         damage = int(input())
-        dragon_get_damage(damage)
-        if not dragon_is_alive():
+        enemy.get_damage(damage)
+        if not enemy.is_alive():  # удалить из списка мертвого врага
+            enemy.final_cry()
+            enemy_list.pop(0)
+        if not enemy_list:  # проверить пуст ли список врагов
             finish = True
+
     print('You win!')
 
 
