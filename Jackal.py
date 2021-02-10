a3270412b57f46a1763875c14d42a3211f9e8f23
BlackBond88 <78538203+BlackBond88@users.noreply.github.com>
2021-02-11 01:41:39 +0200

    созданы картинки

--- Jackal.py
+++ Jackal.py
@@ -10,11 +10,28 @@ class Cells:
     def __init__(self, name):
         self.name = name
 
-    def create(self):
-        """ Создает клетку поля """
-        picture_list = picture_load()
+    def cell_draw(self, i):
+        """ рисует клетки """
+        self.i = i
+        image_name = 'Image/' + self.name + '.png'
+        image_cell = pygame.image.load(image_name)
+        rect_cell = pygame.draw.rect(screen, BLACK, (100+self.i*100, 100, 100, 100))
+        screen.blit(image_cell, rect_cell)
 
+#rect = pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.heigth))
 
+def picture_create():
+    """ Создает клетку поля """
+    picture_list = picture_load()   #
+    cells = []
+    i = 1
+    for image in picture_list:      # создает клетки на поле
+        cell = Cells(image)
+        cell.cell_draw(i)
+        i += 1
+        cells.append(cell)
+
+    return cells
 
 
 def game_main():
@@ -22,6 +39,11 @@ def game_main():
 
     finished = False
     clock = pygame.time.Clock()     # скорость анимаций
+
+    cells = picture_create()                # создает массив картинок
+
+    #for cell in cells:                      # рисует клетки
+     #   cell.cell_draw()
 
     while not finished:             # запуск игры (цикл)
         clock.tick(FPS)
@@ -31,7 +53,7 @@ def game_main():
                 finished = True
 
         pygame.display.update()     # обновление экрана
-        screen.fill(WHITE)          # закрашивает экран черным
+        #screen.fill(WHITE)          # закрашивает экран
 
     pygame.quit()                   # выход из игры
 
